cb183a64ce7f81417b90a6f5a97d6211a8ea57df
Amanuel <amanuel0000@gmail.com>
2021-08-18 00:08:03 +0300

    navigation set pd up

--- App.js
+++ App.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import {Provider} from "react-redux";
+import { Provider } from "react-redux";
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import HomeScreen from './screens/HomeScreen';
 import { store } from './store';
-
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import MapScreen from './screens/MapScreen';
 
 export default function App() {
+
+  const Stack = createNativeStackNavigator();
   return (
-   <Provider store={store}>
-      <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-   </Provider>
+    <Provider store={store}>
+      <NavigationContainer>
+        <SafeAreaProvider>
+          <Stack.Navigator>
+            <Stack.Screen name="HomeScreen" component={HomeScreen} options={{ headerShown: false }} />
+            <Stack.Screen name="MapScreen" component={MapScreen} options={{ headerShown: false }} />
+          </Stack.Navigator>
+        </SafeAreaProvider>
+      </NavigationContainer>
+    </Provider>
   );
 }
 
